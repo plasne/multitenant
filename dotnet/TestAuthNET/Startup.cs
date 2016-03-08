895e1409a6c3fda33c269a7700ad5075465ec08a
plasne <plasne@iname.com>
2016-03-08 18:20:54 -0500

    dotnet samples

---
+++ dotnet/TestAuthNET/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+using System.Security.Cryptography;
+using System.ServiceModel.Security.Tokens;
+
+[assembly: OwinStartup(typeof(TestAuthNET.Startup))]
+
+namespace TestAuthNET
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+
+            // JWT Bearer
+            if (ConfigurationManager.ConnectionStrings["JWTKey"] != null)
+            {
+                string key_s = ConfigurationManager.ConnectionStrings["JWTKey"].ConnectionString;
+                byte[] key_b = System.Text.Encoding.UTF8.GetBytes(key_s);
+                app.UseJwtBearerAuthentication(
+                    new Microsoft.Owin.Security.Jwt.JwtBearerAuthenticationOptions
+                    {
+                        TokenValidationParameters = new TokenValidationParameters
+                        {
+                            IssuerSigningToken = new BinarySecretSecurityToken(key_b),
+                            RequireExpirationTime = true,
+                            ValidateIssuer = true,
+                            ValidIssuer = "http://testauth.plasne.com",
+                            ValidateAudience = false
+                        }
+                    });
+            }
+
+        }
+
+
+
+    }
+}
---
+++ dotnet/TestAuthNET/WebAPIService.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Security.Claims;
+using System.ServiceModel.Security.Tokens;
+using System.Web.Http;
+
+namespace TestAuthNET
+{
+    public class WebAPIService : ApiController
+    {
+
+        [DataContract]
+        public class Reply
+        {
+
+            [DataMember]
+            public List<string> claims;
+
+            [DataMember]
+            public string message;
+        
+            public Reply()
+            {
+                claims = new List<string>();
+            }
+        }
+
+        [Authorize]
+        [HttpGet, Route("hello")]
+        public Reply Hello()
+        {
+            Reply reply = new Reply();
+
+            // write out the claims
+            ClaimsIdentity identity = User.Identity as ClaimsIdentity;
+            foreach (Claim claim in identity.Claims)
+            {
+                reply.claims.Add("[" + claim.Type + " " + claim.Value);
+            }
+
+            // write the message
+            Claim user = identity.Claims.First(claim => claim.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier", StringComparison.InvariantCultureIgnoreCase));
+            reply.message = "Hello from the WebAPI Service. Happy to see you, " + ((user != null) ? user.Value : "unknown") + ".";
+
+            return reply;
+        }
+
+
+
+    }
+}
