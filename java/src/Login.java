ca5c86f1bd990cb93e81a1623dc4665488d5e3a8
Chris Joakim <christopher.joakim@gmail.com>
2016-04-06 12:23:53 -0400

    Added file res/multitenant.properties.  Dynamically reading these values in the Login Servlet.

--- java/src/Login.java
+++ java/src/Login.java
@@ -3,23 +3,43 @@ import java.io.*;
 import java.net.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
+import java.util.Properties;
 
 // Extend HttpServlet class
 public class Login extends HttpServlet {
 
+  protected static Properties properties = new Properties();
+
   public void init() throws ServletException
   {
+    try {
+      if (properties.size() < 1) {
+        InputStream stream = this.getClass().getResourceAsStream("multitenant.properties");
+        properties.load(stream);
+        String authority = (String) properties.get("login.authority");
+        System.out.println("authority: " + authority);
+      }
+    }
+    catch (Exception ex) {
+      ex.printStackTrace();
+      throw new ServletException(ex); 
+    }  
   }
 
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
   {
-
     // variables
-    String authority = "https://login.windows.net/common";
-    String clientId = "36bda7c5-cc23-4618-9e09-e710b2357818";
-    String redirect = "http://pelasne-java.southcentralus.cloudapp.azure.com:8080/multitenant/token";
-    String state = "random";
-    String resource = "https://graph.windows.net/";
+    // String authority = "https://login.windows.net/common";
+    // String clientId = "36bda7c5-cc23-4618-9e09-e710b2357818";
+    // String redirect = "http://pelasne-java.southcentralus.cloudapp.azure.com:8080/multitenant/token";
+    // String state = "random";
+    // String resource = "https://graph.windows.net/";
+    
+    String authority = (String) properties.get("login.authority");
+    String clientId  = (String) properties.get("login.clientId");
+    String redirect  = (String) properties.get("login.redirect");
+    String state     = (String) properties.get("login.state");
+    String resource  = (String) properties.get("login.resource");
 
     // write auth cookie
     response.addCookie(new Cookie("authstate", state));
